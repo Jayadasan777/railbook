d40728f01098cbecac89253aa84dae4665a2b375
dasan <dasan@gmail.com>
2026-06-18 23:53:56 +0530

    Fix CORS for Vercel

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://railbook-mauve.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
